2b68c371d722c40843ea91faeb2207965856ef76
ImKsa0203 <nicocasanova0203@gmail.com>
2025-05-21 08:33:36 -0300

    termometro display

--- Assets/Scripts/Items/Tools/Thermometer.cs
+++ Assets/Scripts/Items/Tools/Thermometer.cs
@@ -1,27 +1,39 @@
+using System;
 using Objects.Clues;
 using UnityEngine;
 using Interfaces;
+using Patterns;
+using TMPro;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 namespace Items.Tools
 {
     public class Thermometer : Tool
     {
-        private float _temperature = 15;
-        public LayerMask clueLayerMask;
+        [SerializeField] private TMP_Text text;
+        [SerializeField] private SO_ThermClue defaultTemp;
+        [SerializeField] private LayerMask clueLayerMask;
+        [SerializeField] private float cooldownTime;
+        [SerializeField] private float lerpSpeed = 0.5f;
+        
+        private float _temperature = 25;
+        private float _targetTemperature = 25;
+        private Cooldown _cooldown = new();
 
-        [SerializeField] private MeshRenderer screen;
+        private void OnEnable()
+        {
+            UpdateTemperatureDisplay();
+        }
 
-        [SerializeField] private Material highMaterial;
-        [SerializeField] private Material lowMaterial;
-        [SerializeField] private Material negativeMaterial;
-    
         public override void Use()
         {
-        
+            
         }
 
         private void Update()
         {
+            print("Temp" + _temperature + " Target: " + _targetTemperature);
             Collider[] hits = Physics.OverlapSphere(transform.position, 1, clueLayerMask);
             foreach (var hit in hits)
             {
@@ -31,27 +43,45 @@ namespace Items.Tools
                     SO_Clue type = clue.GetClueProvided;
                     if (type is SO_ThermClue tempClue)
                     {
-                        var (minTemp, maxTemp) = tempClue.GetValue;
-                        float targetTemp = Random.Range(minTemp, maxTemp);
-                        _temperature = Mathf.Lerp(_temperature, targetTemp, Time.deltaTime);
-                        SetMaterial();
+                        UpdateTargetTemperature(tempClue.GetValue);
+                        UpdateTemperature();
                         return;
                     }
                 }
             }
-
-            _temperature = Mathf.Lerp(_temperature, 15f, Time.deltaTime);
-            SetMaterial();
+            
+            UpdateTargetTemperature(defaultTemp.GetValue);
+            UpdateTemperature();
         }
 
-        private void SetMaterial()
+        private void UpdateTemperature()
         {
-            screen.material = _temperature switch
+            _temperature = Mathf.Lerp(_temperature, _targetTemperature, Time.deltaTime * lerpSpeed);
+            UpdateTemperatureDisplay();
+        }
+
+        private void UpdateTemperatureDisplay()
+        {
+            text.SetText(_temperature.ToString("F1") + " \u00b0C");
+        }
+
+        private void UpdateTargetTemperature((int, int) temp)
+        {
+            if (_targetTemperature >= temp.Item1 && _targetTemperature <= temp.Item2)
             {
-                >= 10 => highMaterial,
-                >= 0 => lowMaterial,
-                _ => negativeMaterial
-            };
+                if (_cooldown.IsReady)
+                {
+                    NewTargetTemperature(temp);
+                }
+                return;
+            }
+            NewTargetTemperature(temp);
+        }
+
+        private void NewTargetTemperature((int, int) temp)
+        {
+            _targetTemperature = Random.Range((float)temp.Item1, temp.Item2);
+            _cooldown.StartCooldown(cooldownTime);
         }
     }
 }

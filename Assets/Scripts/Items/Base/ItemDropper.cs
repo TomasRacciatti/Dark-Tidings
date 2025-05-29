760551ddc307dff10172af1c0a89dafb849cea80
ImKsa0203 <nicocasanova0203@gmail.com>
2025-05-29 12:32:51 -0300

    tmp y ajuste dropper

--- Assets/Scripts/Items/Base/ItemDropper.cs
+++ Assets/Scripts/Items/Base/ItemDropper.cs
@@ -9,18 +9,18 @@ namespace Items.Base
     {
         private static ItemDropper _instance;
         [SerializeField] private GameObject itemPrefab;
-        
+
         private void Awake()
         {
             _instance = this;
             Hide();
         }
-        
+
         public static void Show()
         {
             _instance.gameObject.SetActive(true);
         }
-        
+
         public static void Hide()
         {
             _instance.gameObject.SetActive(false);
@@ -32,8 +32,16 @@ namespace Items.Base
             SlotUI fromSlotUI = fromItemUI.GetComponentInParent<SlotUI>();
             if (fromSlotUI == null) return;
 
-            GameObject itemObject = Instantiate(itemPrefab, GameManager.Player.transform.position, Quaternion.identity);
+            GameObject itemObject = Instantiate(itemPrefab, GameManager.Player.transform.position + 1.2f * Vector3.up,
+                Quaternion.identity);
             itemObject.GetComponent<ItemPrefab>().SetItemAmount(fromItemUI.itemAmount);
+            
+            Rigidbody rb = itemObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddForce((GameManager.Player.transform.forward + 0.8f * Vector3.up).normalized * 3.5f, ForceMode.Impulse);
+            }
+            
             fromSlotUI.InventoryUI.Inventory.SetItemByIndex(fromSlotUI.SlotIndex, new ItemAmount());
             Hide();
         }

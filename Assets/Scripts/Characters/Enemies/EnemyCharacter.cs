3893e1149b4ea568bec67f9d053b9810ac442a1a
ImKsa0203 <nicocasanova0203@gmail.com>
2025-06-03 04:47:13 -0300

    mapa items y fix enemy y dummy

--- Assets/Scripts/Characters/Enemies/EnemyCharacter.cs
+++ Assets/Scripts/Characters/Enemies/EnemyCharacter.cs
@@ -1,7 +1,17 @@
+using System;
+
 namespace Characters.Enemies
 {
     public class EnemyCharacter : Character
     {
-        
+        private void Start()
+        {
+            _healthComponent.OnDeath += Death;
+        }
+
+        private void Death()
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
